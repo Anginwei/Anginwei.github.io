cae0a6c99565bebfcb6f9b69d363b86931d66e90
SenYuanWei <anginwei@foxmail.com>
2015-06-07 09:05:08 +0800

    update

--- project/start-or-not/index.js
+++ project/start-or-not/index.js
@@ -1,5 +1,23 @@
-$(document).ready(function () {
-	var now = new Date();
+$(document).ready(function() {
 
-	$(".timeout").text(27 - now.getDay());
+	reflash();
 });
+
+function reflash() {
+	var now = new Date(),
+	   t;
+
+	$(".day").text(26 - now.getDate());
+	$(".hour").text(23 - now.getHours());
+	$(".min").text(addZero(59 - now.getMinutes()));
+	$(".sec").text(addZero(59 - now.getSeconds()));
+
+	t = setTimeout("reflash()", 1000);
+}
+
+function addZero (i) {
+	if (i<10) {
+		i = "0" + i;
+	}
+	return i;
+}
